6af4c42bd9051742d5f34da6df1f57ee14f5d551
Jag_k <30597878+jag-k@users.noreply.github.com>
2024-11-04 23:19:49 +0400

    Initial setup for DuckDuckGo Image Search extension
        - Add .editorconfig for consistent code formatting with 4-space indent for .ts, .tsx, .js, .jsx files.
        - Add .gitignore to exclude node_modules directory.
        - Create CHANGELOG.md with a placeholder for the initial version.
        - Create README.md with a brief description of the DuckDuckGo Image Search extension.
        - Add assets for extension icon and metadata image.
        - Add package-lock.json and package.json with necessary dependencies and scripts for development and publishing.
        - Implement the main functionality in src/search-image.tsx to search and display images using DuckDuckGo.
        - Define TypeScript configurations in tsconfig.json.
        - Create utility files for caching, constants, helpers, and search logic.

---
+++ raycast-env.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Retry Count - The number of times to retry a failed search (default: ) */
+  "retries": string,
+  /** Sleep Time - The time (in milliseconds) to sleep between retries (default: 2000) */
+  "sleep": string,
+  /** Locale - The locale of the search (default: en-us) */
+  "locale": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `search-image` command */
+  export type SearchImage = ExtensionPreferences & {
+  /** Moderate Search - Moderate the search results (may cause some results to be removed) */
+  "moderate": boolean,
+  /** License - The license of the images to search */
+  "license": "" | "Any" | "Public" | "Share" | "ShareCommercially" | "Modify" | "ModifyCommercially"
+}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `search-image` command */
+  export type SearchImage = {}
+}
+
